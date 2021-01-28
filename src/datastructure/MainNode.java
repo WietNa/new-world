9a6ade6a11a35e9857ad9d8d4e307022aefe3344
Wiet Na <vuanthanhtung76@gmail.com>
2021-01-28 13:31:16 +0700

    commit

--- src/datastructure/MainNode.java
+++ src/datastructure/MainNode.java
@@ -1,11 +1,12 @@
 package datastructure;
 
 import org.w3c.dom.Node;
+import javax.xml.crypto.Data;
 
 public class MainNode {
     private static Node Head = null;
     public static void main(String[] args) {
-        Node node = new Node(data, 7);
+        Node node = new Node(Data:7);
         Head = node;
         System.out.println("AAAAAAAAAA");
     }
