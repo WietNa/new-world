f8dfc5a18a6b755579d7a794f25e9c0e3101b9a6
Wiet Na <vuanthanhtung76@gmail.com>
2021-01-28 13:28:13 +0700

    first commit

---
+++ src/datastructure/MainNode.java
@@ -0,0 +1,12 @@
+package datastructure;
+
+import org.w3c.dom.Node;
+
+public class MainNode {
+    private static Node Head = null;
+    public static void main(String[] args) {
+        Node node = new Node(data, 7);
+        Head = node;
+        System.out.println("AAAAAAAAAA");
+    }
+}
--- src/student.java
+++ src/student.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class student {
+    static int Sum(int n) {
+        if (n == 1) return 1;
+        return n + Sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Tong so can tinh: ");
+
+    }
 }
