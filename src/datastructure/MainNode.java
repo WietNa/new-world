b08e6ae29a60e415eebbaaa7599d244e9e66d0c2
Wiet Na <vuanthanhtung76@gmail.com>
2021-01-28 16:16:36 +0700

    commit

--- src/datastructure/MainNode.java
+++ src/datastructure/MainNode.java
@@ -4,9 +4,34 @@ public class MainNode {
     private static Node Head = null;
     public static void main(String[] args) {
         Node node = new Node(3);
-            Head = node;
+        Head = node;
         Node node1 = new Node(6);
-            Head.Next = node1;
-                System.out.println("AAAAAAAAAA");
+        Head = node1;
+        Head.Next = node;
+        Node node2 = new Node (5);
+        node2.Next = Head;
+        Head = node2;
+        Node node3 = new Node(9);
+        node3.Next = node;
+        Head.Next = node3;
+        /* xuat ra danh sach
+            Node p = Head;
+            while (p != null) {
+                p = p.Next;
+            }
+         */
+/* xoa dau
+    p = Head;
+    Head = Head.Next;
+    p = null;
+ */
+        Node p = Head;
+        Node q = p.Next;
+        while (q.Next != null) {
+            p = p.Next;
+            q = q.Next;
+        }
+        p.Next = null;
+                System.out.println("Hello");
         }
     }
